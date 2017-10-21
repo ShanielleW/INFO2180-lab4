18bfbb6eabcf324e987b292f85bbe64bd50d30ce
ShanielleW <shanielle.williams@hotmail.com>
2017-10-21 00:58:36 -0500

    Exercise 2

--- maze.js
+++ maze.js
@@ -4,12 +4,25 @@ Created by Shanielle A. Williams - 620085201 */
 window.onload = main;
     
 function main() {
-    /*------------ Exercise 1 ---------------*/
+    /*-------------------- Exercise 1 --------------------*/
     
-    let wallColor = document.getElementById("boundary1");
-    wallColor.onmouseover = function() {
-        this.className = "boundary youlose";
-    });
+	let wallColor = document.getElementById("boundary1");
+	wallColor.onmouseover = function() {
+        	this.className = "boundary youlose";
+    	});
     
-    /*---------------------------------------*/
+    /*-----------------------------------------------------*/
+
+    /*---------------------- Exercise 2 -------------------*/
+    	let wall = document.querySelectorAll(".boundary");
+    	for(x = 0; x < wall.length;x++){
+		wall[x].addEventListener("mouseover",function() {
+			for(x = 0; x < wall.length;x++){
+				document.getElementById("status").textContent = "Sorry You Lose!!";
+				wall[x].setAttribute("class", "boundary you lose");
+			}
+		});
+    	}
+
+    /*------------------------------------------------------*/
 }
